7f1eb163ed248fc4ebac4ab858e4c68a432cd287
Josué Iván <josvanrh09@gmail.com>
2020-10-23 16:40:39 -0500

    Adding styles and content to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,12 @@ import './Home.scss';
 
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home">
+      <div className="accent-element"></div>
+      <div className="primary-element"></div>
+      <h2>Blog made with React.js</h2>
+      <h3>This blog is a example made with React.js and Axios</h3>
+      <cite>Made by Josué Iván Rodríguez Hernández</cite>
     </div>
   );
 }
